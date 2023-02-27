a168c6f1024640a913d4b904ce4c45b461126e15
crystian97 <icrystianveloso@gmail.com>
2023-02-27 15:36:18 -0300

    feat:initial config

---
+++ src/styles/global.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+*{
+    box-sizing: border-box;
+    padding: 0;
+    margin:0
+}
+body{
+    background-color: #363447;
+    padding: 7rem 7.688rem;
+}
+`;
---
+++ src/styles/themes/default.js
@@ -0,0 +1,9 @@
+export const defaultTheme = {
+  white: "#FFFFFF",
+  gray: "#D9D9D9",
+  shadow: "#1E1E1E",
+  "violet-500": "#363447",
+  "violet-300": "#4A4556",
+  "violet-400": "#292738",
+  green: "#81FBB8",
+};
